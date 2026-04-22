1bc5737ef64d7a18138592fd9df79771c023ff8f
DmitriBogdanov <dm.bogdanov2000@gmail.com>
2026-04-23 00:01:46 +0300

    feat: Add approximate float comparator.

---
+++ include/tmc/functional/approx.hpp
@@ -0,0 +1,55 @@
+// SPDX-FileCopyrightText: Copyright (c) 2026 - present, Dmitri Bogdanov
+// SPDX-FileCopyrightText: https://github.com/DmitriBogdanov/too-many-containers
+//
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+// Content: Floating-point wrapper for comparing with tolerance
+
+#include <cmath>    // std::isfinite, std::abs()
+#include <compare>  // std::partial_ordering
+#include <concepts> // std::floating_point<>, std::same_as<>
+#include <limits>   // std::numeric_limits<>
+
+
+namespace tmc {
+
+// Floating-point comparison is a partial ordering
+static_assert(std::same_as<decltype(1.0 <=> 2.0), std::partial_ordering>);
+static_assert(std::same_as<decltype(1.f <=> 2.f), std::partial_ordering>);
+
+// Comparator that compares values as approximate using `epsilon` tolerance
+// (values with a difference below tolerance are considered to be equal).
+//
+// This behaviour preserves partial ordering.
+// Template type parameter can be omitted since it's deducible from the tolerance.
+//
+template <auto epsilon = std::numeric_limits<double>::epsilon()>
+    requires std::floating_point<decltype(epsilon)>
+struct approx {
+    using value_type = decltype(epsilon);
+
+    constexpr static value_type tolerance = epsilon;
+
+    value_type value{};
+
+    // Non-explicit constructor enables heterogenous comparison with other types (they get converted for comparison)
+    constexpr approx(value_type value) noexcept : value(value) {}
+
+    // Approximate comparator
+    [[nodiscard]] constexpr std::partial_ordering operator<=>(approx other) noexcept {
+        static_assert(std::three_way_comparable<value_type, std::partial_ordering>);
+
+        const auto eval_lhs_is_finite         = [&] { return std::isfinite(this->value); };
+        const auto eval_rhs_is_finite         = [&] { return std::isfinite(other.value); };
+        const auto eval_delta_below_tolerance = [&] { return std::abs(this->value - other.value) < approx::tolerance; };
+
+        if (eval_lhs_is_finite() and eval_rhs_is_finite() and eval_delta_below_tolerance())
+            return std::partial_ordering::equivalent;
+
+        return std::partial_order(this->value, other.value);
+    }
+};
+
+} // namespace tmc
