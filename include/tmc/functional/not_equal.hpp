6f912c608690a8ccab882c4ab0038441db7e3100
DmitriBogdanov <dm.bogdanov2000@gmail.com>
2026-05-01 01:06:54 +0300

    refactor: Move transparent comparators to a granular header structure.

---
+++ include/tmc/functional/not_equal.hpp
@@ -0,0 +1,29 @@
+// SPDX-FileCopyrightText: Copyright (c) 2026 - present, Dmitri Bogdanov
+// SPDX-FileCopyrightText: https://github.com/DmitriBogdanov/too-many-containers
+//
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include <type_traits> // true_type
+#include <utility>     // forward()
+
+namespace tmc {
+
+struct not_equal {
+    
+    template <class L, class R>
+    constexpr decltype(auto) operator()(L&& lhs, R&& rhs) const 
+        noexcept(noexcept(std::forward<L>(lhs) != std::forward<R>(rhs)))
+    {
+        return std::forward<L>(lhs) != std::forward<R>(rhs);
+    }
+
+    using is_transparent = std::true_type;
+    
+};
+
+// Note: This could be an alias to `std::not_equal_to<>`. Re-implementation is motivated by compile times.
+//       <type_traits> + <utility> are very lightweight, ~10 times faster than including <functional>.
+
+} // namespace tmc
