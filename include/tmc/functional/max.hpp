a6aa1d0214bb480010387c30811c49a7a20761ad
DmitriBogdanov <dm.bogdanov2000@gmail.com>
2026-05-10 21:54:29 +0300

    feat: Implement arithmetic function objects.

---
+++ include/tmc/functional/max.hpp
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: Copyright (c) 2026 - present, Dmitri Bogdanov
+// SPDX-FileCopyrightText: https://github.com/DmitriBogdanov/too-many-containers
+//
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include <utility> // forward()
+
+#include <tmc/functional/less.hpp>
+
+namespace tmc {
+
+struct max {
+    
+    template <class L, class R>
+    constexpr decltype(auto) operator()(L&& lhs, R&& rhs) const
+        noexcept(noexcept(tmc::less{}(rhs, lhs) ? std::forward<L>(lhs) : std::forward<R>(rhs)))
+    {
+        return tmc::less{}(rhs, lhs) ? std::forward<L>(lhs) : std::forward<R>(rhs);
+    }
+    
+};
+
+} // namespace tmc
---
+++ include/tmc/functional/minus.hpp
@@ -0,0 +1,26 @@
+// SPDX-FileCopyrightText: Copyright (c) 2026 - present, Dmitri Bogdanov
+// SPDX-FileCopyrightText: https://github.com/DmitriBogdanov/too-many-containers
+//
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+#include <utility> // forward()
+
+namespace tmc {
+
+struct minus {
+    
+    template <class L, class R>
+    constexpr decltype(auto) operator()(L&& lhs, R&& rhs) const
+        noexcept(noexcept(std::forward<L>(lhs) - std::forward<R>(rhs)))
+    {
+        return std::forward<L>(lhs) - std::forward<R>(rhs);
+    }
+    
+};
+
+// Note: This could be an alias to `std::minus<>`. Re-implementation is motivated by compile times.
+//       <utility> is very lightweight, ~10-20 times faster than including <functional>.
+
+} // namespace tmc
